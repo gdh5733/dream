d69cf98a935e15db13d4b650f84ca2122912a6ab
gdh5733 <573308611@qq.com>
2020-06-17 00:21:59 +0800

    boot框架

---
+++ code/before-code/src/main/java/com/dream/starting/StartingApplication.java
@@ -0,0 +1,15 @@
+package com.dream.starting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+
+public class StartingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartingApplication.class, args);
+    }
+
+}
